708d9ddcdbaab076a1c898751a6cadc6c3b5a126
Khanturov Daniil <soloexploder@gmail.com>
2021-04-14 16:47:33 +0300

    Initial Commit

---
+++ RandomPerson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomPerson
+//
+//  Created by Даниил Хантуров on 14.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
